20f923500b10564f421a7ac3890107df33c4cfec
oka311119 <katoko.318@gmail.com>
2023-03-16 22:23:03 +0900

    feat: post hello-world in slack
    fix: commnet

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+const { WebClient } = require("@slack/web-api");
+
+(async () => {
+    // Import the dotenv package
+    require("dotenv").config();
+
+    // OAuth token
+    const token = process.env.TOKEN;
+    // #channel
+    const channel = process.env.CHANNEL;
+    // message
+    const text = "*Hello World*";
+
+    const client = new WebClient(token);
+    const response = await client.chat.postMessage({ channel, text });
+
+    console.log(response);
+})();
